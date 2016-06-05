df073a662a71cc22c7f27eeaa9acfdac2d5ccb27
DaAmazing1 <alex@diokmedjian.com>
2016-06-05 01:22:05 -0400

    Enhancement: added comparision operators for cost, attack and defense

--- client/card-database/dist/scripts/app-9bfb4363bd.js
+++ client/card-database/dist/scripts/app-9bfb4363bd.js
@@ -149,25 +149,65 @@
 
   function CardFilters(cardService) {
     var vm = this;
+    // hit the api for the teams
     cardService
       .teams()
       .then(function(teams){
         vm.teams = teams;
       });
+    // hit the api for the card types
     cardService
       .cardTypes()
       .then(function(cardTypes){
         vm.cardTypes = cardTypes;
       });
+
+    //hit the api for a list of powers
     cardService
       .powers()
       .then(function(powers){
         vm.powers = powers;
       });
+
+    // create a blank query object
     vm.query = {};
+
+    // Gets fired with the submission of the filters
     vm.performQuery = function(){
+      // first thing to check is for empty query parameters, well just delete
+      // them.
+      for(var property in vm.query){
+        if (vm.query.hasOwnProperty(property)) {
+          if(property === "" || property === null){
+            delete vm.query[property];
+          }
+        }
+      }
+
+      // now lets handle the comparision operators for attack, defense and cost
+      ['atk', 'def', 'cost'].forEach(function(prop){
+        if(vm.hasOwnProperty(prop)){
+          vm.query[prop] = {};
+          vm.query[prop][vm[prop + 'Op']] = vm[prop];
+        }
+      });
+
+      // now we can query the api for the results
       cardService
         .query(vm.query);
+    };
+
+    // fired when user clicks on the reset form button
+    vm.performReset = function(){
+      vm.query = {};
+      ['atk', 'def', 'cost'].forEach(function(prop){
+        if(vm.hasOwnProperty(prop)){
+          delete vm[prop];
+        }
+        if(vm.hasOwnProperty(prop + 'Op')){
+          delete vm[prop + 'Op'];
+        }
+      });
     };
   }
 })(window.angular);
@@ -194,5 +234,5 @@
   }
 })(window.angular);
 
-angular.module("card").run(["$templateCache", function($templateCache) {$templateCache.put("components/card-filters/card-filters.html","<form><div class=\"form-group\"><label for=\"cardName\">Card Name</label> <input type=\"text\" class=\"form-control\" id=\"cardName\" ng-model=\"vm.query.name\"></div><div class=\"form-group\"><label for=\"cardType\">Card Type</label><am-multiselect class=\"input-lg\" multiple=\"true\" ms-selected=\"There are {{vm.query.types.length}} selected\" ng-model=\"vm.query.type\" ms-header=\"Select card types\" options=\"type for type in vm.cardTypes\" template-url=\"multiselect.tmpl.html\" change=\"selected()\"></am-multiselect></div><div class=\"form-group\"><label for=\"team\">Team</label><am-multiselect class=\"input-lg\" multiple=\"true\" ms-selected=\"There are {{vm.query.team.length}} selected\" ng-model=\"vm.query.team\" ms-header=\"Select teams\" options=\"team for team in vm.teams\" template-url=\"multiselect.tmpl.html\" change=\"selected()\"></am-multiselect></div><div class=\"form-group\"><label for=\"power\">Power</label><am-multiselect class=\"input-lg\" multiple=\"true\" ms-selected=\"There are {{vm.query.powers.length}} selected\" ng-model=\"vm.query.powers\" ms-header=\"Select powers\" options=\"power for power in vm.powers\" template-url=\"multiselect.tmpl.html\" change=\"selected()\"></am-multiselect></div><div class=\"form-group\"><label for=\"cardText\">Card Text</label> <input type=\"text\" class=\"form-control\" id=\"cardText\" ng-model=\"vm.query.rules\"></div><div class=\"form-group\"><label for=\"cardCost\">Card Cost</label> <input type=\'text\"\' class=\"form-control\" id=\"cardCost\" ng-model=\"vm.query.cost\"></div><div class=\"form-group\"><label for=\"atk\">Attack</label> <input type=\"text\" class=\"form-control\" id=\"atk\" ng-model=\"vm.query.atk\"></div><div class=\"form-group\"><label for=\"def\">Defense</label> <input type=\"text\" class=\"form-control\" id=\"def\" ng-model=\"vm.query.def\"></div><button type=\"submit\" class=\"btn btn-primary\" ng-click=\"vm.performQuery()\">Search</button> <button type=\"reset\" class=\"btn btn-default\">Reset</button></form>");
+angular.module("card").run(["$templateCache", function($templateCache) {$templateCache.put("components/card-filters/card-filters.html","<form name=\"filters\" novalidate=\"\" ng-submit=\"vm.performQuery()\"><div class=\"form-group\"><label for=\"cardName\">Card Name</label> <input type=\"text\" class=\"form-control\" id=\"cardName\" ng-model=\"vm.query.name\"></div><div class=\"form-group\"><label for=\"cardType\">Card Type</label><am-multiselect class=\"input-lg\" multiple=\"true\" ms-selected=\"There are {{vm.query.types.length}} selected\" ng-model=\"vm.query.type\" ms-header=\"Select card types\" options=\"type for type in vm.cardTypes\" template-url=\"multiselect.tmpl.html\" change=\"selected()\"></am-multiselect></div><div class=\"form-group\"><label for=\"team\">Team</label><am-multiselect class=\"input-lg\" multiple=\"true\" ms-selected=\"There are {{vm.query.team.length}} selected\" ng-model=\"vm.query.team\" ms-header=\"Select teams\" options=\"team for team in vm.teams\" template-url=\"multiselect.tmpl.html\" change=\"selected()\"></am-multiselect></div><div class=\"form-group\"><label for=\"power\">Power</label><am-multiselect class=\"input-lg\" multiple=\"true\" ms-selected=\"There are {{vm.query.powers.length}} selected\" ng-model=\"vm.query.powers\" ms-header=\"Select powers\" options=\"power for power in vm.powers\" template-url=\"multiselect.tmpl.html\" change=\"selected()\"></am-multiselect></div><div class=\"form-group\"><label for=\"cardText\">Card Text</label> <input type=\"text\" class=\"form-control\" id=\"cardText\" ng-model=\"vm.query.rules\"></div><div class=\"form-group\"><label for=\"cardCost\">Card Cost</label><div class=\"row\"><div class=\"col-xs-6\"><select class=\"form-control\" ng-model=\"vm.costOp\"><option value=\"$eq\">&#61;</option><option value=\"$gt\">&gt;</option><option value=\"$gte\">&gt;&#61;</option><option value=\"$lt\">&lt;</option><option value=\"$lte\">&lt;&#61;</option></select></div><div class=\"col-xs-6\"><input type=\'text\"\' class=\"form-control\" id=\"cardCost\" ng-model=\"vm.cost\"></div></div></div><div class=\"form-group\"><label for=\"atk\">Attack</label><div class=\"row\"><div class=\"col-xs-6\"><select class=\"form-control\" ng-model=\"vm.atkOp\"><option value=\"$eq\">&#61;</option><option value=\"$gt\">&gt;</option><option value=\"$gte\">&gt;&#61;</option><option value=\"$lt\">&lt;</option><option value=\"$lte\">&lt;&#61;</option></select></div><div class=\"col-xs-6\"><input type=\"text\" class=\"form-control\" id=\"atk\" ng-model=\"vm.atk\"></div></div></div><div class=\"form-group\"><label for=\"def\">Defense</label><div class=\"row\"><div class=\"col-xs-6\"><select class=\"form-control\" ng-model=\"vm.defOp\"><option value=\"$eq\">&#61;</option><option value=\"$gt\">&gt;</option><option value=\"$gte\">&gt;&#61;</option><option value=\"$lt\">&lt;</option><option value=\"$lte\">&lt;&#61;</option></select></div><div class=\"col-xs-6\"><input type=\"text\" class=\"form-control\" id=\"def\" ng-model=\"vm.def\"></div></div></div><button type=\"submit\" class=\"btn btn-primary\">Search</button> <button type=\"button\" class=\"btn btn-default\" ng-click=\"vm.performReset()\">Reset</button></form>");
 $templateCache.put("components/card-results/card-results.html","<div id=\"cardResults\" ui-grid=\"vm.gridOptions\" class=\"card-results-grid\" ui-grid-expandable=\"\"></div>");}]);
--- client/card-database/src/app/components/card-filters/card-filters.js
+++ client/card-database/src/app/components/card-filters/card-filters.js
@@ -10,25 +10,65 @@
 
   function CardFilters(cardService) {
     var vm = this;
+    // hit the api for the teams
     cardService
       .teams()
       .then(function(teams){
         vm.teams = teams;
       });
+    // hit the api for the card types
     cardService
       .cardTypes()
       .then(function(cardTypes){
         vm.cardTypes = cardTypes;
       });
+
+    //hit the api for a list of powers
     cardService
       .powers()
       .then(function(powers){
         vm.powers = powers;
       });
+
+    // create a blank query object
     vm.query = {};
+
+    // Gets fired with the submission of the filters
     vm.performQuery = function(){
+      // first thing to check is for empty query parameters, well just delete
+      // them.
+      for(var property in vm.query){
+        if (vm.query.hasOwnProperty(property)) {
+          if(property === "" || property === null){
+            delete vm.query[property];
+          }
+        }
+      }
+
+      // now lets handle the comparision operators for attack, defense and cost
+      ['atk', 'def', 'cost'].forEach(function(prop){
+        if(vm.hasOwnProperty(prop)){
+          vm.query[prop] = {};
+          vm.query[prop][vm[prop + 'Op']] = vm[prop];
+        }
+      });
+
+      // now we can query the api for the results
       cardService
         .query(vm.query);
+    };
+
+    // fired when user clicks on the reset form button
+    vm.performReset = function(){
+      vm.query = {};
+      ['atk', 'def', 'cost'].forEach(function(prop){
+        if(vm.hasOwnProperty(prop)){
+          delete vm[prop];
+        }
+        if(vm.hasOwnProperty(prop + 'Op')){
+          delete vm[prop + 'Op'];
+        }
+      });
     };
   }
 })(window.angular);
